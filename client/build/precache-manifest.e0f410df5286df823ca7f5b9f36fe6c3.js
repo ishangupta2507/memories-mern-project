88580f1e21ba3269e5c962f508c08c2cdbc117e3
ishangupta2507 <164048495+ishangupta2507@users.noreply.github.com>
2024-07-29 13:37:07 +0530

    build

---
+++ client/build/precache-manifest.e0f410df5286df823ca7f5b9f36fe6c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b33591bdad5fa7dce4a28f6611db3ccb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c7bd371c934e466d370",
+    "url": "/static/css/main.d93f5428.chunk.css"
+  },
+  {
+    "revision": "95af1275785e8b5ab5be",
+    "url": "/static/js/2.1148188a.chunk.js"
+  },
+  {
+    "revision": "4b2087d7283ac9223e6e3252595f9784",
+    "url": "/static/js/2.1148188a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7bd371c934e466d370",
+    "url": "/static/js/main.24fedd67.chunk.js"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
